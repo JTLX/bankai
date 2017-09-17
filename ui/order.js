1ca93bded9f50ceecb2becb5cf0a7828c07b1e59
Joseph Tang <josephtang765@gmail.com>
2017-09-17 09:29:32 -0700

    add timer

--- ui/order.js
+++ ui/order.js
@@ -117,7 +117,7 @@ updateTimer();
 setInterval(updateTimer, 1000);
 setTimeout(function() {
     addMember(1);
-}, 8000)
+}, 6000)
 
 function parse_query_string(query) {
   var vars = query.split("&"); var query_string = {}; for (var i = 0; i < vars.length; i++) { var pair = vars[i].split("="); if (typeof query_string[pair[0]] === "undefined") { query_string[pair[0]] = decodeURIComponent(pair[1]); } else if (typeof query_string[pair[0]] === "string") { var arr = [query_string[pair[0]], decodeURIComponent(pair[1])]; query_string[pair[0]] = arr; } else { query_string[pair[0]].push(decodeURIComponent(pair[1])); } } return query_string;
